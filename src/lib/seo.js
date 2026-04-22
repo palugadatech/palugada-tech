d57e407c738e42f02d2f7baac04333aa731827e8
palugada.tech <palugadatech.official@gmail.com>
2026-04-22 17:28:19 +0700

    update default image url

--- src/lib/seo.js
+++ src/lib/seo.js
@@ -1,6 +1,6 @@
 export const SITE_NAME = 'palugada.tech';
 export const BASE_URL = 'https://palugadatech.my.id';
-export const DEFAULT_OG_IMAGE = `${BASE_URL}/logo_palugada_tech.png.jpg`;
+export const DEFAULT_OG_IMAGE = `${BASE_URL}/logo_palugada_tech.png`;
 
 export function absoluteUrl(pathname = '/') {
   const normalizedPath = pathname === '/' ? '/' : `/${pathname.replace(/^\/+/, '')}`;
